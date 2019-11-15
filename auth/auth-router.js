767df93c9df31ff95b4c48778ca790c8795df338
Mark Gowen <mark.gowen78@gmail.com>
2019-11-15 08:31:25 -0800

    created auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,17 +1,45 @@
-require('dotenv').config()
-const router = require('express').Router();
-const bcrypt = require('bcryptjs')
+require("dotenv").config();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-const JWT = require('./token')
-const Users = require('./auth-model')
+const JWT = require("./token");
+const Users = require("./auth-model");
 
-
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      const token = JWT.generateToken(saved);
+      res.status(201).json({ user: saved, token });
+    })
+    .catch(err => {
+      console.log("Error..", err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = JWT.generateToken(user);
+        res.status(200).json({ message: `Welcome ${user.username}!`, token });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch(err => {
+      console.log("Error..", err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
