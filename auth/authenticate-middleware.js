aee6594edf74c8f543530d868bdc55ca79033dd1
Mark Gowen <mark.gowen78@gmail.com>
2019-11-15 08:36:32 -0800

    created Authentication middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,22 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, process.env.ENCRYPTION_KEY, (err, decodedToken) => {
+      if (err) {
+        console.log("Failed Verification", err);
+        res.status(401).json({ message: "You must be logged in to do that" });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "No Token Provided" });
+  }
 };
